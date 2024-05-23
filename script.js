bc698e8ed0e1eafea6536b715ec7ac0cb116c4ac
ppinkpanther <156557755+ppinkpanther@users.noreply.github.com>
2024-05-22 19:11:15 -0500

    Add ' ' as placeholder text in confirm password

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const password = document.querySelector('#password');
 const passwordConfirm = document.querySelector('#confirm-password');
 
+function checkValidity() {
+    return password == passwordConfirm;
+}
+
+
+
+console.log(checkValidity());
