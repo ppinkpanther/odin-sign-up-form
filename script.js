e6396f92b670f4c28c70ef35c0d91d945edf3393
ppinkpanther <156557755+ppinkpanther@users.noreply.github.com>
2024-05-25 15:41:52 -0500

    Change validity of passwords when are/are not ===
    The new tag on the ul holding the two password
    elements lets us use just one event listener for
    both in the JS, meaning the inputs update in real
    time.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const password = document.querySelector('#password');
 const passwordConfirm = document.querySelector('#confirm-password');
+const passwordInputs = document.querySelector('.password-inputs');
+
 
 function checkValidity() {
-    return password == passwordConfirm;
+    if(password.value == passwordConfirm.value){
+    password.setCustomValidity('');
+    passwordConfirm.setCustomValidity('');
+   } else {
+    password.setCustomValidity('Passwords do not match');
+    passwordConfirm.setCustomValidity('Passwords do not match');
+   }
+
+   console.log(password.checkValidity(), passwordConfirm.checkValidity())
 }
 
+passwordInputs.addEventListener('input', () => {
+    console.log(password.value, passwordConfirm.value);
+    checkValidity();
+})
 
-
-console.log(checkValidity());
